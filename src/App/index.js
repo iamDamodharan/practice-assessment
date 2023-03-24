72c49455796dd608520e9eba66acd3bb4e410698
Damodharan <iamdamodharan@gmail.com>
2023-03-24 17:32:50 +0530

    react router include

--- src/App/index.js
+++ src/App/index.js
@@ -1,11 +1,15 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../Container/Home";
-import '../index.css';
 
 const App = () => {
   return (
-    <Home />
+    <BrowserRouter>
+      <Routes>
+          <Route index element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
-};
+}
 
 export default App;
